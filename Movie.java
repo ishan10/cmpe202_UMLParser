7c7879404e31c5450e8c2a56ab3af43a8ccf33d7
ishan10 <ikpandya10@gmail.com>
2017-02-18 10:39:56 -0800

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,18 @@
+package com.parser;
+
+public class Movie {
+
+	public String script;
+	public Cast actor;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The best movie EVER.!!");
+	}
+
+	public void showDetails(){
+		System.out.println("A epic saga of love and deciet");
+	}
+	
+}
